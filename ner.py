5da3e876785c9b7d2255230f90179e8d61e9931d
Dharani8713 <dharani.mp13@gmail.com>
2025-06-20 22:14:29 +0530

    Update ner.py

--- ner.py
+++ ner.py
@@ -2,8 +2,13 @@
 
 import spacy
 
-# Load SpaCy's English NER model
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 def extract_entities(text):
     """
